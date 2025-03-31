37acacf164f0051b1ecf5fd89c20df2d4cee99a0
Eva Terriault <eterriault@cmaisonneuve.qc.ca>
2025-03-30 20:24:15 -0400

    Update classe BD

--- src/pyforum/bd.py
+++ src/pyforum/bd.py
@@ -1,112 +1,58 @@
-# Exemple d'utilisation des méthodes BD dans les cas d'utilisation
-
 class BD:
     def __init__(self):
+        self.utilisateurs = []
+        self.forums = []
+        self.publications = []
+        self.commentaires = []
+        self.utilisateurs_forums = {}
         print("Base de données initialisée.")
-
-    @staticmethod
-    def sauvegarder_utilisateur(utilisateur):
-        """Simule la sauvegarde d'un utilisateur dans la base de données.
-
-        Paramètre:
-            utilisateur (Utilisateur): L'utilisateur à sauvegarder.
-        """
+    
+    def sauvegarder_utilisateur(self, utilisateur):
+        for i, existing_user in enumerate(self.utilisateurs):
+            if existing_user.id == utilisateur.id:
+                self.utilisateurs[i] = utilisateur  # Mise à jour de l'utilisateur existant
+                print(f"[Simulé] Mise à jour de l'utilisateur: {utilisateur}")
+                return
+        self.utilisateurs.append(utilisateur)  # Ajout de l'utilisateur s'il n'existe pas
         print(f"[Simulé] Sauvegarde de l'utilisateur: {utilisateur}")
-
-    @staticmethod
-    def sauvegarder_forum(forum):
-        """Simule la sauvegarde d'un forum dans la base de données.
-
-        Paramètre:
-            forum (Forum): Le forum à sauvegarder.
-        """
+    
+    def sauvegarder_forum(self, forum):
+        for i, existing_forum in enumerate(self.forums):
+            if existing_forum.id == forum.id:
+                self.forums[i] = forum  # Mise à jour du forum existant
+                print(f"[Simulé] Mise à jour du forum: {forum}")
+                return
+        self.forums.append(forum)  # Ajout du forum s'il n'existe pas
         print(f"[Simulé] Sauvegarde du forum: {forum}")
 
-    @staticmethod
-    def sauvegarder_publication(forum_id, publication):
-        """Simule la sauvegarde d'une publication dans un forum.
+    def sauvegarder_publication(self, publication):
+        for i, existing_publication in enumerate(self.publications):
+            if existing_publication.id == publication.id:
+                self.publications[i] = publication  # Mise à jour de la publication existante
+                print(f"[Simulé] Mise à jour de la publication: {publication}")
+                return
+        self.publications.append(publication)  # Ajout de la publication si elle n'existe pas
+        print(f"[Simulé] Sauvegarde de la publication: {publication}")
 
-        Paramètres:
-            forum_id (int): L'identifiant du forum.
-            publication (Publication): La publication à sauvegarder.
-        """
-        print(f"[Simulé] Sauvegarde de la publication '{publication}' dans le forum {forum_id}")
+    def sauvegarder_commentaire(self, commentaire):
+        for i, existing_commentaire in enumerate(self.commentaires):
+            if existing_commentaire.id == commentaire.id:
+                self.commentaires[i] = commentaire  # Mise à jour du commentaire existant
+                print(f"[Simulé] Mise à jour du commentaire: {commentaire}")
+                return
+        self.commentaires.append(commentaire)  # Ajout du commentaire s'il n'existe pas
+        print(f"[Simulé] Sauvegarde du commentaire: {commentaire}")
 
-    @staticmethod
-    def sauvegarder_commentaire(publication_id, commentaire):
-        """Simule la sauvegarde d'un commentaire dans une publication.
+    def obtenir_forum_par_nom(self, nom_forum):
+        return next((forum for forum in self.forums if forum.nom == nom_forum), None)
+    
+    def obtenir_utilisateur_par_nom(self, nom_utilisateur):
+        return next((utilisateur for utilisateur in self.utilisateurs if utilisateur.nom == nom_utilisateur), None)
+    
+    def obtenir_publication_par_titre(self, titre_publication):
+        return next((publication for publication in self.publications if publication.titre == titre_publication), None)
 
-        Paramètres:
-            publication_id (int): L'identifiant de la publication.
-            commentaire (Commentaire): Le commentaire à sauvegarder.
-        """
-        print(
-            f"[Simulé] Sauvegarde du commentaire '{commentaire}' pour "
-            f"la publication {publication_id}"
-        )
+     
 
-    @staticmethod
-    def sauvegarder_utilisateur_forum(forum_id, utilisateur):
-        """Simule l'ajout d'un utilisateur à un forum.
 
-        Paramètres:
-            forum_id (int): L'identifiant du forum.
-            utilisateur (Utilisateur): L'utilisateur à ajouter.
-        """
-        print(f"[Simulé] Ajout de l'utilisateur {utilisateur} au forum {forum_id}")
-
-    @staticmethod
-    def charger_utilisateurs():
-        """Simule le chargement des utilisateurs.
-
-        Retourne:
-            list: La liste des utilisateurs.
-        """
-        print("[Simulé] Chargement des utilisateurs...")
-        return []
-
-    @staticmethod
-    def charger_forums():
-        """Simule le chargement des forums.
-
-        Retourne:
-            list: La liste des forums.
-        """
-        print("[Simulé] Chargement des forums...")
-        return []
-
-    @staticmethod
-    def charger_publications(forum_id):
-        """Simule le chargement des publications d'un forum.
-
-        Paramètre:
-            forum_id (int): L'identifiant du forum.
-
-        Retourne:
-            list: La liste des publications.
-        """
-        print(f"[Simulé] Chargement des publications du forum {forum_id}...")
-        return []
-
-    @staticmethod
-    def charger_commentaires(publication_id):
-        """Simule le chargement des commentaires d'une publication.
-
-        Paramètre:
-            publication_id (int): L'identifiant de la publication.
-
-        Retourne:
-            list: La liste des commentaires.
-        """
-        print(f"[Simulé] Chargement des commentaires pour la publication {publication_id}...")
-        return []
-
-    @staticmethod
-    def charger_utilisateurs_forums():
-        """Simule le chargement des utilisateurs inscrits aux forums.
-
-        Retourne:
-            list: La liste des inscriptions des utilisateurs aux forums.
-        """
-        print("[Simulé] Chargement des utilisateurs des forums...")
-        return []
+    
